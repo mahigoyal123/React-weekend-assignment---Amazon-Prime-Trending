3d75815d4f230ce7588b8159afa67aafbb1be9f2
manisha goyal <48145474+mahigoyal123@users.noreply.github.com>
2021-06-19 21:56:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,36 @@
-import React, { useState} from "react";
-import  slides  from '../data'
-import '../styles/App.css';
+import React, { useState } from 'react'
 
-const App = () => {
+const useStateWithLocalStorage = localStoragekey=> {
+  const [text, setText]= useState(
+  localStorage.getItem(localStoragekey) || ''
+  );
 
-  const [currSlides, setCurrSlides] = useState(0)
 
-  const clickPrev=()=>{
+  React.useEffect(()=> {
+    localStorage.setItem(localStoragekey, text);
+
+  }, [text]);
+
+  return [text, setText];
+};
+
+function App() {
+const [text, setText] = useStateWithLocalStorage(
+  'myvalue'
+);
+
+  const handleclick= (e)=> {
+    setText(e.target.value);
+  }
+
   
-       currSlides >0 && setCurrSlides(currSlides-1);
-       }
-  
-  
-  const clickNext=()=>{
-       currSlides <slides.length-1 && setCurrSlides(currSlides+1);
-       }
-  
-  const clickRestart = () => {
-      setCurrSlides(0);
-  }     
-
-
   return (
-    <>
-     <div className="App">
-      <div className="carousel">
-            <h1 data-testid="title">{slides[currSlides].title}</h1>
-            <p data-testid="text">{slides[currSlides].text}</p>
-        </div>
-        <button onClick={clickPrev} disabled={currSlides===0?true:false} data-testid="button-prev" >Prev</button>
-        <button onClick={clickNext} disabled={currSlides===slides.length-1?true:false} data-testid="button-next">Next</button>
-        <button onClick={clickRestart} disabled={currSlides===0?true:false} data-testid="button-restart">Restart</button>
-    </div> 
-    
-    </>
+    <div>
+      <h2>{text}</h2>
+      <input type="text" value={text} onChange={handleclick}/>
+      
+    </div>
   )
 }
 
-
-export default App;
+export default App
